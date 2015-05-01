e2541a109d73c231e9104229e78b77c50d5c9e44
contoracutar <contoracutar@softbank.ne.jp>
2015-05-01 09:12:57 +0900

    複数タッチでの拡大縮小追加

--- Scripts/MainCamera.cs
+++ Scripts/MainCamera.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class MainCamera : Mover {
 
 	Point point;
+	float distance = 8, speed = 0.05f;
 	void Awake () {
 		point = GameObject.Find ("Point").GetComponent<Point> ();
 	}
@@ -12,7 +13,9 @@ public class MainCamera : Mover {
 	}
 	
 	void Update () {
+		distance -= TouchController.scaleMoveLength * speed;
+		distance = Constrain (distance, 8, 30);
 		transform.rotation = Quaternion.Slerp (transform.rotation, point.GetBaseRotation () * RotationX (0.1f), 1);
-		transform.position = point.pos + AxisY () + AxisZ () * -8;
+		transform.position = point.pos + AxisY () + AxisZ () * -distance;
 	}
 }
--- Scripts/TouchController.cs
+++ Scripts/TouchController.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class TouchController : Mover {
 
-	Vector3 standPos;
+	Vector3 standPos, mouseOrTouchVec;
+	bool [] touches = new bool[2];
+	float scaleLength;
+
+	public static float scaleMoveLength;	//二本指でスワイプで前フレームとの二点間の距離の差分
 	public static bool touchTrigger;		//押してるかどうか
-	public static Vector3 swipeVec;			//スワイプのベクトル
+	public static Vector3 swipeVec;			//スワイプの移動ベクトル
 	public static Vector3 swipeVecNormal;	//上の単位ベクトル
 	public static Vector3 touchMoveVec;		//前フレームとのベクトルの差分
 	public static Vector3 rayHitPoint;		//タッチした先の位置を取得
@@ -16,15 +20,16 @@ public class TouchController : Mover {
 
 	void Start () {
 		touchTrigger = false;
-		swipeVec = swipeVecNormal = Vector3.zero;
+		swipeVec = swipeVecNormal = mouseOrTouchVec = Vector3.zero;
 		rayHitObj = null;
 	}
 	
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
+			mouseOrTouchVec = Input.GetTouch (0).position;	//or Input.mousePosition;
 			touchTrigger = true;
-			pos = prevPos = standPos = Input.mousePosition;
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			pos = prevPos = standPos = mouseOrTouchVec;
+			Ray ray = Camera.main.ScreenPointToRay (mouseOrTouchVec);
 			RaycastHit hit;
 			if (Physics.Raycast (ray, out hit)) {
 				rayHitObj = hit.collider.gameObject;
@@ -47,5 +52,24 @@ public class TouchController : Mover {
 		} else {
 			pos = prevPos = swipeVec = swipeVecNormal = touchMoveVec = Vector3.zero;
 		}
+
+		float leng;
+		for (int i = 0; i < 2; i++) {
+			if (Input.GetTouch (i).phase == TouchPhase.Began) {
+				touches [i] = true;
+				scaleLength = leng = Length (Input.GetTouch (0).position - Input.GetTouch (1).position);
+			}
+			if (Input.GetTouch (i).phase == TouchPhase.Ended) {
+				touches [i] = false;
+			}
+		}
+		if (touches [0] && touches [1]) {
+			leng = Length (Input.GetTouch (0).position - Input.GetTouch (1).position);
+			scaleMoveLength = leng - scaleLength;
+			scaleLength = leng;
+		} else {
+			scaleMoveLength = 0;
+		}
+		Debug.Log (touchMoveVec);
 	}
 }
